62bb1677702a4fba202e33344350360981d74573
unknown <stellazen.xd@gmail.com>
2024-03-19 17:16:25 -0300

    feat: inicialização e instalação de dependências

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Form from "./components/form"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-red-600">Increva-se</h1>
+      <Form />
+    </>
+  )
+}
+
+export default App
