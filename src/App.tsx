2c564aa4a77b58f5f783e4b9a30ea14de5e8e095
StellaZen <stellazen.xd@gmail.com>
2024-04-08 21:01:02 -0300

    feat: construção form

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,14 @@ function App() {
 
   return (
     <>
-      <h1 className="text-red-600">Increva-se</h1>
-      <Form />
+      <h1 className="text-red-600 font-medium">Increva-se</h1>
+      <p>Assine nossa Newslewtter e mantenha-se informado.</p>
+      <div>
+        <Form />
+      </div>
+      <p>
+        Ao se inscrever, você passará a receber as melhores dicas e novidades.
+      </p>
     </>
   )
 }
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,6 +1,23 @@
 const Form = () => {
     return(
-        <div>Form</div>
+        <form>
+            <div>
+                <label>Nome</label>
+                <input type="text" name="" id="" placeholder="Digite seu nome" />
+            </div>
+            <div>
+                <label>Email</label>
+                <input type="email" name="" id="" placeholder="Digite seu email" />
+            </div>
+            <div>
+                <a href="#">Termos de uso</a>
+                <div>
+                    <input type="checkbox" name="" id="" />
+                    <label>Concordo com os termos</label>
+                </div>
+            </div>
+            <button>Cadastrar</button>
+        </form>
     )
 }
 
