370dae62fcad22b4de2537c6777f6289631fe61e
unknown <stellazen.xd@gmail.com>
2024-05-02 16:11:48 -0300

    feat: criação do form e validator

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,17 @@ function App() {
 
   return (
     <>
-      <h1 className="text-red-600">Increva-se</h1>
-      <Form />
+      <div className="bg-slate-900 min-h-screen w-full flex flex-col items-center justify-center">
+        <h1 className="font-bold text-[2rem] text-white">
+          Increva-se
+        </h1>
+        <p className="text-white">Assine nossa Newsletter e mantenha-se informado</p>
+        <div className="w-96 mt-4 bg-stone-200 px-4 py-4 rounded-lg">
+          <Form />
+        </div>
+        <p className="text-slate-100 text-xs w-96 mt-2 text-center">Ao se Increver, você passará a receber os nossos e-mails com as melhores dicas, novidades e ofertas.</p>
+        
+      </div>
     </>
   )
 }
